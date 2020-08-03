8e0037989f19a38b1b4a7aa6db88fc0ec330d90e
Mission: St. Louis <mstl@Missions-MBP-45.attlocal.net>
2020-08-02 21:11:31 -0500

    Sections 1-3

---
+++ orbit-report/src/app/satellite.ts
@@ -0,0 +1,18 @@
+import { TYPED_NULL_EXPR } from '@angular/compiler/src/output/output_ast';
+
+export class Satellite {
+    name: string;
+    orbitType: string;
+    type: string;
+    operational: boolean;
+    launchDate: string;
+
+   constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
+       this.name = name;
+       this.type = type;
+       this.launchDate = launchDate;
+       this.orbitType = orbitType;
+       this.operational = operational;
+   }
+
+}
