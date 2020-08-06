8f2c21d51e5d509edb1ffafb0ec0663d2be27f25
Mission: St. Louis <mstl@Missions-MacBook-Pro-45.local>
2020-08-05 19:30:02 -0500

    step 6 & 8 and bonus 1

--- orbit-report/src/app/app.component.ts
+++ orbit-report/src/app/app.component.ts
@@ -8,12 +8,14 @@ import { Satellite } from './satellite';
 })
 export class AppComponent {
   title = 'orbit-report';
-
   sourceList: Satellite[];
+  displayList: Satellite[];
 
   constructor() {
     this.sourceList = [];
+    this.displayList = [];
     let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
+
  
     window.fetch(satellitesUrl).then(function(response) {
        response.json().then(function(data) {
@@ -28,22 +30,25 @@ export class AppComponent {
           // TODO: loop over satellites
           // TODO: create a Satellite object using new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
           // TODO: add the new Satellite object to sourceList using: this.sourceList.push(satellite);
- 
-       }.bind(this));
+       // make a copy of the sourceList to be shown to the user
+        this.displayList = this.sourceList.slice(0);
+      }.bind(this));
     }.bind(this));
- 
+  }
+
+  search(searchTerm: string): void {
+    let matchingSatellites: Satellite[] = [];
+    searchTerm = searchTerm.toLowerCase();
+    for(let i=0; i < this.sourceList.length; i++) {
+       let name = this.sourceList[i].name.toLowerCase();
+       if (name.indexOf(searchTerm) >= 0) {
+          matchingSatellites.push(this.sourceList[i]);
+       }
+    }
+    // assign this.displayList to be the array of matching satellites
+    // this will cause Angular to re-make the table, but now only containing matches
+    this.displayList = matchingSatellites;
  }
-
-  // constructor() {
-  //   this.sourceList = [
-  //     new Satellite("SiriusXM", "Communication", "2009-03-21", "LOW", true),
-  //     new Satellite("Cat Scanner", "Imaging", "2012-01-05", "LOW", true),
-  //     new Satellite("Weber Grill", "Space Debris", "1996-03-25", "HIGH", false),
-  //     new Satellite("GPS 938", "Positioning", "2001-11-01", "HIGH", true),
-  //     new Satellite("ISS", "Space Station", "1998-11-20", "LOW", true),
-  //   ];
-  // }
-
 
 }
 
